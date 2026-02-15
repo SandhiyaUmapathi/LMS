f33df57a1db334c6878aa60efebcc9e8284f2e02
SandhiyaUmapathi <sandhiya.u2023@vitstudent.ac.in>
2026-02-15 19:21:21 +0530

    Added login feature

---
+++ login.py
@@ -0,0 +1,6 @@
+class LoginSystem:
+    def login(self, username, password):
+        if username == "admin" and password == "1234":
+            print("Login Successful")
+        else:
+            print("Invalid Credentials")
